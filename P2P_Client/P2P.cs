96370e6a6318b66f895cf365bf212eb6440f84bb
Christian Holcombe <christian220h@outlook.com>
2020-07-27 08:49:31 -0400

    Transfers with lost packets

--- P2P_Client/P2P.cs
+++ P2P_Client/P2P.cs
@@ -1,12 +1,9 @@
 ﻿using P2P_Utilities;
 using System;
-using System.Collections;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,7 +13,9 @@ namespace P2P
     class P2P
     {
         public static Utilities utils;
-        public static string testPath = @"C:\Users\cholcombe\Documents\GitHub\P2P-File-Transfer\Range - Why Generalists Triumph in a Specialized World.pdf";
+        //public static string testPath = @"C:\Users\cholcombe\Documents\GitHub\P2P-File-Transfer\Range - Why Generalists Triumph in a Specialized World.pdf";
+        //public static string testPath = @"C:\Users\cholcombe\Documents\GitHub\P2P-File-Transfer\Test.txt";
+        public static string testPath = @"C:\Users\cholcombe\Documents\GitHub\P2P-File-Transfer\firmware-bltouch-for-z-homing.bin";
         [STAThread]
         static async Task Main(string[] args)
         {
@@ -32,7 +31,7 @@ namespace P2P
                 utils = new Utilities(IPAddress.Loopback);
                 await Client();
             }
-        }              
+        }
 
         static async Task Client()
         {
@@ -46,18 +45,18 @@ namespace P2P
             stopwatch.Start();
             //Console.WriteLine(path);
             if (!File.Exists(path)) { File.Create(path); }
-            Thread.Sleep(100);
+            while (!File.Exists(path)) { Thread.Sleep(100); }
             using (FileStream fileStream = new FileStream(path, FileMode.Truncate, FileAccess.Write))
             {
                 await foreach (var result in utils.RecieveFileData())
                 {
+                    await fileStream.FlushAsync();
                     var temp = result.Where(x => x != BitConverter.GetBytes(' ').First()).ToArray();
-                    if (Encoding.Default.GetString(temp) == "stop") { Console.WriteLine($"\"{temp}\" received"); break; }
+                    if (Encoding.Default.GetString(temp) == "stop1234") { Console.WriteLine($"\n\"{Encoding.Default.GetString(temp)}\" received"); break; }
                     await fileStream.WriteAsync(temp);
 
                     Console.CursorLeft = 0;
                     Console.Write($"{bytesReceived += temp.Length}/{utils.fileSize} bytes received");
-                    await fileStream.FlushAsync();
                 }
             }
             stopwatch.Stop();
@@ -68,16 +67,16 @@ namespace P2P
         {
             await utils.SetupServer();
             await utils.InitServer(testPath);
-            
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            await foreach(var result in utils.SendData(testPath))
+            await foreach (var result in utils.SendData(testPath))
             {
                 Console.CursorLeft = 0;
                 Console.Write($"{result}/{utils.fileSize} bytes sent");
             }
             Thread.Sleep(500);
-            await utils.SendData(Encoding.Default.GetBytes("stop"));
+            await utils.SendData(Encoding.Default.GetBytes("stop1234"));
             stopwatch.Stop();
             Console.WriteLine($"\n\nFile sent in {stopwatch.Elapsed}s");
         }
--- Utilities.cs
+++ Utilities.cs
@@ -1,165 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace P2P_Utilities
-{	
-	public class Utilities
-	{
-		public IPAddress IPAddress;
+{
+    public class Utilities
+    {
+        public IPAddress IPAddress;
 
-		public TcpListener serverListener;
-		public Socket serverSocket;
+        public TcpListener serverListener;
+        public Socket serverSocket;
 
-		public TcpClient client;
-		public Stream clientStream;
+        public TcpClient client;
+        public Stream clientStream;
 
-		public int BUFFER_SIZE = 1024;
+        public int BUFFER_SIZE = 1024;
 
-		public long fileSize = 0;
-		public string fileName = "";
-		public Utilities() {}
-		public Utilities(IPAddress ip)
-		{
-			IPAddress = ip;
-		}
-		public async Task SetupServer()
+        public long fileSize = 0;
+        public string fileName = "";
+        public Utilities() { }
+        public Utilities(IPAddress ip)
         {
-			//serverListener = new TcpListener(IPAddress.Parse(new WebClient().DownloadString("http://icanhazip.com")), 5001);
-			//serverListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 5001);
-			//serverListener.Start();
-			Console.Write("Establishing connection... ");
-			serverListener = new TcpListener(IPAddress.Loopback, 80);
-			serverListener.Start();
-
-			serverSocket = await serverListener.AcceptSocketAsync();
-			//serverSocket = await serverListener.AcceptSocketAsync();
-			Console.Write("Connected to destination client\n");
-		}		
-		public async Task InitServer(string file)
-        {			
-			fileSize = new FileInfo(file).Length;
-			await SendData(Encoding.ASCII.GetBytes(fileSize.ToString()));
-			fileName = new FileInfo(file).Name;
-			await SendData(Encoding.Default.GetBytes(fileName));
-		}
-
-		public async Task SetupClient()
-        {
-			client = new TcpClient();			
-			Console.Write("Establishing connection... ");
-			await client.ConnectAsync(IPAddress, 80);
-			Console.Write("Connected to source server\n\n");
-		}
-		public async Task InitClient()
-        {
-			byte[] tempArr = await RecieveInitData();
-			fileSize = Convert.ToInt64(Encoding.ASCII.GetString(tempArr));
-			tempArr = await RecieveInitData();
-			fileName = Encoding.ASCII.GetString(tempArr).Trim();
+            IPAddress = ip;
         }
-		public async IAsyncEnumerable<byte[]> RecieveFileData()
-		{
-			clientStream = client.GetStream();
-			byte[] byteArr = new byte[BUFFER_SIZE];
-			byte[] result;
-			long bytesReceived = 0;
-			do
-			{
-				//Console.WriteLine("Awaiting stream read");
-				int length = await clientStream.ReadAsync(byteArr, 0, byteArr.Length);
-				bytesReceived += length;
-
-				result = byteArr[0..(length-1)];
-
-				await clientStream.WriteAsync(Encoding.Default.GetBytes($"Success"));
-				//Console.WriteLine("Awaiting write)");
-				yield return result;
-			} while (bytesReceived < fileSize);
-			Console.WriteLine($"packet only {result.Length}");
-		}
-
-		public async Task<byte[]> RecieveInitData()
-		{
-			clientStream = client.GetStream();
-			byte[] byteArr = new byte[BUFFER_SIZE];
-			for(int i = 0; i < BUFFER_SIZE; i++) { byteArr[i] = BitConverter.GetBytes(' ').First(); }
-			await clientStream.ReadAsync(byteArr, 0, byteArr.Length);
-			await clientStream.WriteAsync(Encoding.Default.GetBytes($"Success"));
-			return byteArr;
-		}
-
-		public async IAsyncEnumerable<long> SendData(string path)
+        public async Task SetupServer()
         {
-			long bytesSent = 0;
-			byte[] tempArr = new byte[BUFFER_SIZE];
-			await foreach (var result in ChunkData(path))
-			{
-				//Console.WriteLine("\nAwaiting send data");
-				//Console.WriteLine(Encoding.Default.GetString(result));
-				await serverSocket.SendAsync(result,SocketFlags.None);// TODO: do not send and recieve empty packets
-				//Console.WriteLine("Awaiting recieve");
-				var res = await serverSocket.ReceiveAsync(tempArr, SocketFlags.None);
-				yield return (bytesSent += result.Length);
-			}
+            //serverListener = new TcpListener(IPAddress.Parse(new WebClient().DownloadString("http://icanhazip.com")), 5001);
+            //serverListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 5001);
+            //serverListener.Start();
+            Console.Write("Establishing connection... ");
+            serverListener = new TcpListener(IPAddress.Loopback, 80);
+            serverListener.Start();
+
+            serverSocket = await serverListener.AcceptSocketAsync();
+            //serverSocket = await serverListener.AcceptSocketAsync();
+            Console.Write("Connected to destination client\n");
         }
-		public async Task SendData(byte[] byteArr)
-		{
-			byte[] tempArr = new byte[20];
-			//Console.WriteLine("\nAwaiting send data 1");
-			await serverSocket.SendAsync(byteArr, SocketFlags.None);
-			Console.WriteLine($"\n\"{Encoding.Default.GetString(byteArr).Trim()}\"");
-			//Console.WriteLine("Awaiting recieve 1");
-			await serverSocket.ReceiveAsync(tempArr, SocketFlags.None);
-			//tempArr = (byte[])tempArr.Where(x => BitConverter.ToString(new byte[] { x }) != "32"); //read byte space
-			Console.WriteLine(Encoding.ASCII.GetString(tempArr).Trim()+"\n");
-		}
+        public async Task InitServer(string file)
+        {
+            fileSize = new FileInfo(file).Length;
+            await SendData(Encoding.ASCII.GetBytes(fileSize.ToString()));
+            fileName = new FileInfo(file).Name;
+            await SendData(Encoding.Default.GetBytes(fileName));
+        }
 
-		public async IAsyncEnumerable<byte[]> ChunkData(string path)
-		{
-			var stream = new MemoryStream();
-			byte[] buffer = new byte[BUFFER_SIZE];
-			long bytesRead = 0;
-			do
-			{
-				int length = await stream.ReadAsync(buffer, (int)bytesRead, buffer.Length);
-				if (length == 0) { break; }
-				var result = buffer[0..(length-1)];
-				bytesRead += length;
-				await stream.FlushAsync();
-				yield return result;
-			} while (true);
-		}
-		public async IAsyncEnumerable<byte[]> ChunkString(string str)
-		{
-			//while (true)
-			//{
-			byte[] tempData = new byte[BUFFER_SIZE]; //Stores one chunk of data
-			byte[] dataBytes = Encoding.Default.GetBytes(str); //Stores all data TODO: convert to filestream
-			int extraBytes = 0; //Track any extra bytes after we have exhausted full chunks
-			int dataChunks = dataBytes.Length / BUFFER_SIZE; //Determine how many chunks are in the 'file'
-			for (int i = 0; i <= dataChunks; i++) //Loop for all dataChunks and one more for extraBytes
-			{
-				if (i == dataChunks) //Last iteration
-				{
-					extraBytes = dataBytes.Length - dataChunks * BUFFER_SIZE;
-					if (extraBytes == 0) break; //Break if no extra bytes
-				}
-				int endPoint = dataChunks > 0 ? (BUFFER_SIZE * (i != dataChunks ? (i + 1) : i)) : 0;
-				int frontPoint = i == 0 ? 0 : (BUFFER_SIZE * i);
+        public async Task SetupClient()
+        {
+            client = new TcpClient();
+            Console.Write("Establishing connection... ");
+            await client.ConnectAsync(IPAddress, 80);
+            Console.Write("Connected to source server\n\n");
+        }
+        public async Task InitClient()
+        {
+            byte[] tempArr = await RecieveInitData();
+            fileSize = Convert.ToInt64(Encoding.ASCII.GetString(tempArr));
+            tempArr = await RecieveInitData();
+            fileName = Encoding.ASCII.GetString(tempArr).Trim();
+        }
+        public async IAsyncEnumerable<byte[]> RecieveFileData()
+        {
+            clientStream = client.GetStream();
+            byte[] byteArr = new byte[BUFFER_SIZE];
+            byte[] result;
+            long bytesReceived = 0;
+            do
+            {
+                //Console.WriteLine("Awaiting stream read");
+                int length = await clientStream.ReadAsync(byteArr, 0, byteArr.Length);
 
-				tempData = dataBytes[frontPoint..(endPoint + extraBytes)]; //Assign tempData to range
+                result = byteArr[0..length];
 
-				yield return tempData;
-			}
-			//}
-		}
-	}
+                await clientStream.WriteAsync(Encoding.Default.GetBytes($"Success"));
+                //Console.WriteLine("Awaiting write)");
+                yield return result;
+            } while (bytesReceived < fileSize);
+            Console.WriteLine($"packet only {result.Length}");
+        }
+
+        public async Task<byte[]> RecieveInitData()
+        {
+            clientStream = client.GetStream();
+            byte[] byteArr = new byte[BUFFER_SIZE];
+            for (int i = 0; i < BUFFER_SIZE; i++) { byteArr[i] = BitConverter.GetBytes(' ').First(); }
+            await clientStream.ReadAsync(byteArr, 0, byteArr.Length);
+            await clientStream.WriteAsync(Encoding.Default.GetBytes($"Success"));
+            return byteArr;
+        }
+
+        public async IAsyncEnumerable<long> SendData(string path)
+        {
+            long bytesSent = 0;
+            byte[] tempArr = new byte[BUFFER_SIZE];
+            await foreach (var result in ChunkData(path))
+            {
+                //Console.WriteLine("\nAwaiting send data");
+                //Console.WriteLine(Encoding.Default.GetString(result));
+                await serverSocket.SendAsync(result, SocketFlags.None);// TODO: do not send and recieve empty packets
+                                                                       //Console.WriteLine("Awaiting recieve");
+                var res = await serverSocket.ReceiveAsync(tempArr, SocketFlags.None);
+                yield return (bytesSent += result.Length);
+            }
+        }
+        public async Task SendData(byte[] byteArr)
+        {
+            byte[] tempArr = new byte[20];
+            //Console.WriteLine("\nAwaiting send data 1");
+            await serverSocket.SendAsync(byteArr, SocketFlags.None);
+            Console.WriteLine($"\n\"{Encoding.Default.GetString(byteArr).Trim()}\"");
+            //Console.WriteLine("Awaiting recieve 1");
+            await serverSocket.ReceiveAsync(tempArr, SocketFlags.None);
+            //tempArr = (byte[])tempArr.Where(x => BitConverter.ToString(new byte[] { x }) != "32"); //read byte space
+            Console.WriteLine(Encoding.ASCII.GetString(tempArr).Trim() + "\n");
+        }
+
+        public async IAsyncEnumerable<byte[]> ChunkData(string path)
+        {
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            using (var file = new BinaryReader(stream))
+            {
+                do
+                {
+                    var tempData = file.ReadBytes(BUFFER_SIZE);
+                    if (tempData.Length < BUFFER_SIZE) { yield return tempData; break; }
+                    await stream.FlushAsync();
+                    yield return tempData;
+                } while (true);
+            }
+        }
+        public async IAsyncEnumerable<byte[]> ChunkString(string str)
+        {
+            //while (true)
+            //{
+            byte[] tempData = new byte[BUFFER_SIZE]; //Stores one chunk of data
+            byte[] dataBytes = Encoding.Default.GetBytes(str); //Stores all data TODO: convert to filestream
+            int extraBytes = 0; //Track any extra bytes after we have exhausted full chunks
+            int dataChunks = dataBytes.Length / BUFFER_SIZE; //Determine how many chunks are in the 'file'
+            for (int i = 0; i <= dataChunks; i++) //Loop for all dataChunks and one more for extraBytes
+            {
+                if (i == dataChunks) //Last iteration
+                {
+                    extraBytes = dataBytes.Length - dataChunks * BUFFER_SIZE;
+                    if (extraBytes == 0) break; //Break if no extra bytes
+                }
+                int endPoint = dataChunks > 0 ? (BUFFER_SIZE * (i != dataChunks ? (i + 1) : i)) : 0;
+                int frontPoint = i == 0 ? 0 : (BUFFER_SIZE * i);
+
+                tempData = dataBytes[frontPoint..(endPoint + extraBytes)]; //Assign tempData to range
+
+                yield return tempData;
+            }
+            //}
+        }
+    }
 }
